040bc0bff4dce50b76b33aa92223b92bd301cb1a
nine <nine.yang.coding@gmail.com>
2018-12-17 14:30:39 +0800

    update component tabbar

---
+++ components/tabbar/tabbar.js
@@ -0,0 +1,17 @@
+/**
+ * created by Nine on 2018/12/17.
+ */
+
+'use strict';
+
+Component({
+    data: {
+        active: 0
+    },
+    methods: {
+        onChange(event) {
+            console.log(event.detail);
+        }
+    }
+
+});
