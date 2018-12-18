d3d14876edd5965eca1abfa0f22120ff95c4e8d4
nine <nine.yang.coding@gmail.com>
2018-12-18 14:21:49 +0800

    finish book

--- components/book/index.js
+++ components/book/index.js
@@ -4,18 +4,40 @@
 
 'use strict';
 
+import Dialog from '../../miniprogram_npm/vant-weapp/dialog/dialog';
+
 Component({
     properties: {
+        showRemove: Boolean
     },
     data: {
-
+        showConfirm: false
     },
     pageLifetimes: {
         show() {
         },
     },
     methods: {
+        onRemove(event) {
+            this.setData({
+                showConfirm: true
+            });
+        },
 
+        onClose(event) {
+            if (event.detail === 'confirm') {
+                // 异步关闭弹窗
+                setTimeout(() => {
+                    this.setData({
+                        showConfirm: false
+                    });
+                }, 1000);
+            } else {
+                this.setData({
+                    showConfirm: false
+                });
+            }
+        }
     }
 
 });
--- pages/bookcase/index.js
+++ pages/bookcase/index.js
@@ -2,7 +2,19 @@ const app = getApp()
 
 Page({
     data: {
-
+        "icon": "edit",
+        "showRemove": false
     },
 
+    onEdit() {
+        let icon = "edit", show = false;
+        if (this.data.icon == "edit") {
+            icon = "success";
+            show = true;
+        }
+        this.setData({
+            "icon": icon,
+            "showRemove": show
+        })
+    },
 });
