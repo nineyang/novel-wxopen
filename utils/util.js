0b7b9e4a183fcd08a71ebafa6d90c7f65713d1cb
nine <nine.yang.coding@gmail.com>
2018-12-19 16:06:56 +0800

    封装 getUserInfo

--- utils/util.js
+++ utils/util.js
@@ -1,19 +1,50 @@
 const formatTime = date => {
-  const year = date.getFullYear()
-  const month = date.getMonth() + 1
-  const day = date.getDate()
-  const hour = date.getHours()
-  const minute = date.getMinutes()
-  const second = date.getSeconds()
+    const year = date.getFullYear()
+    const month = date.getMonth() + 1
+    const day = date.getDate()
+    const hour = date.getHours()
+    const minute = date.getMinutes()
+    const second = date.getSeconds()
 
-  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+    return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
 }
 
 const formatNumber = n => {
-  n = n.toString()
-  return n[1] ? n : '0' + n
+    n = n.toString()
+    return n[1] ? n : '0' + n
+}
+
+
+const getUserInfo = (app, object) => {
+    if (app.globalData.userInfo) {
+        object.setData({
+            userInfo: app.globalData.userInfo,
+            hasUserInfo: true
+        })
+    } else if (this.data.canIUse) {
+        // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+        // 所以此处加入 callback 以防止这种情况
+        app.userInfoReadyCallback = res => {
+            object.setData({
+                userInfo: res.userInfo,
+                hasUserInfo: true
+            })
+        }
+    } else {
+        // 在没有 open-type=getUserInfo 版本的兼容处理
+        wx.getUserInfo({
+            success: res => {
+                app.globalData.userInfo = res.userInfo
+                object.setData({
+                    userInfo: res.userInfo,
+                    hasUserInfo: true
+                })
+            }
+        })
+    }
 }
 
 module.exports = {
-  formatTime: formatTime
+    formatTime: formatTime,
+    getUserInfo: getUserInfo
 }
