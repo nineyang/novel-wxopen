7aac8f3f2509e0dad1d25b309c748eba2987bd1d
nine <nine.yang.coding@gmail.com>
2018-12-19 16:45:36 +0800

    finish finance

--- pages/finance/index.js
+++ pages/finance/index.js
@@ -4,7 +4,22 @@ Page({
     data: {
         userInfo: {},
         hasUserInfo: false,
-        canIUse: wx.canIUse('button.open-type.getUserInfo')
+        canIUse: wx.canIUse('button.open-type.getUserInfo'),
+        type: 'payout',
+        mapper: {
+            payout: 0,
+            income: 1
+        }
     },
+
+    onLoad(options) {
+        this.setData({
+            type: options.type
+        })
+    },
+
+    onChange(event) {
+        console.log(event);
+    }
 
 });
